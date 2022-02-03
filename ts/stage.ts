f15435930dab159de1ade844567c872981d35f72
Matt Deeds <mdeeds@gmail.com>
2022-02-02 18:21:23 -0800

    Fragment shaded

--- ts/stage.ts
+++ ts/stage.ts
@@ -1,9 +1,10 @@
 import * as THREE from "three";
-import { ShaderMaterial } from "three";
 import { KnobTarget } from "./knob";
+import { S } from "./settings";
 import { Synth } from "./synth";
 
 export class Stage extends THREE.Object3D {
+  private materials: THREE.ShaderMaterial[] = [];
   constructor(synth: Synth) {
     super();
 
@@ -17,11 +18,14 @@ export class Stage extends THREE.Object3D {
     for (let i = 0; i < 9; ++i) {
       const x = r * Math.sin(i / 9 * 2 * Math.PI);
       const z = -r * Math.cos(i / 9 * 2 * Math.PI);
-      const ballGeometry = new THREE.IcosahedronBufferGeometry(0.3, 3);
+      const ballGeometry = new THREE.IcosahedronBufferGeometry(
+        0.3, /*detail=*/S.float('s'));
       ballGeometry.translate(0, 0.3, 0);
+      const material = this.getBlobMaterial(new THREE.Color('#885'));
+      this.materials.push(material);
       b = new THREE.Mesh(
         ballGeometry,
-        this.getBlobMaterial(new THREE.Color('#885')));
+        material);
       b.position.set(x, 0, z);
       b.castShadow = true;
       this.add(b);
@@ -70,38 +74,68 @@ export class Stage extends THREE.Object3D {
     const material = new THREE.ShaderMaterial({
       uniforms: {
         color: { value: color },
+        time: { value: 0 },
       },
       vertexShader: `
 uniform vec3 color;
+uniform float time;
+
 varying float v_Density;
 varying vec3 v_Color;
+varying vec4 v_WorldPosition;
+varying float v_Light;
 
 void main() {
   // A unit vector pointing from the object to the camera.
   vec4 worldPosition = modelMatrix * vec4(position, 1);
   worldPosition = worldPosition / worldPosition.w;
   vec3 cameraVector = normalize(cameraPosition - worldPosition.xyz);
-  v_Density = clamp(2.0 * pow(dot(cameraVector, normal), 0.8), 0.0, 1.0);
+  v_Density = clamp(1.5 * pow(dot(cameraVector, normal), 0.9), 0.0, 1.0);
   float light = dot(normal, vec3(0, 1, 0));
-  v_Color = light * color;
+
+  vec3 cf = sin(sin(4.1 * worldPosition.xyz * worldPosition.y + 0.2 * time) * 
+      4.0 + cos(3.2 * worldPosition.yzx * 2.3) * 2.1 * worldPosition.x + 0.314 * time);
+
+  v_Color = color * light * length(cf);
+  v_Light = light;
+  v_WorldPosition = worldPosition;
+
 
   gl_Position = projectionMatrix * modelViewMatrix * 
     vec4(position * 1.1, 1.0);
 }      
       `,
       fragmentShader: `
+uniform vec3 color;
+uniform float time;
+
 varying float v_Density;
 varying vec3 v_Color;
-            
+varying vec4 v_WorldPosition;
+varying float v_Light;
+                  
 void main() {
-  gl_FragColor = vec4(v_Color, v_Density);
-  // gl_FragColor = vec4(v_Normal, 0.5);
+  vec4 worldPosition = v_WorldPosition;
+  vec3 cf = sin(sin(4.1 * worldPosition.xyz * worldPosition.y + 0.2 * time) * 
+      4.0 + cos(3.2 * worldPosition.yzx * 2.3) * 2.1 * worldPosition.x + 0.314 * time);
+ 
+  // vec3 co = v_Color;
+  float light = v_Light;
+  vec3 co = color * light * length(cf);
+
+  gl_FragColor = vec4(co, v_Density);
 }      
       `,
       transparent: true,
     });
 
     return material;
+  }
+
+  update(elapsedS: number) {
+    for (const m of this.materials) {
+      m.uniforms['time'].value = elapsedS;
+    }
   }
 
 
